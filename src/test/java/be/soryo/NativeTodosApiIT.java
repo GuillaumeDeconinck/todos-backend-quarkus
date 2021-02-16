c43f935a2735f97a86df9be5704654d338bed195
Guillaume Deconinck <guillaume@qualifio.com>
2021-02-16 21:03:35 +0100

    feat: init repository

---
+++ src/test/java/be/soryo/NativeTodosApiIT.java
@@ -0,0 +1,9 @@
+package be.soryo;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeTodosApiIT extends TodosApiTest {
+
+    // Execute the same tests but in native mode.
+}
