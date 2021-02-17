78b25b3fdf40212749a6d08f0f27924632ee93d5
Guillaume Deconinck <guillaume@qualifio.com>
2021-02-17 11:20:41 +0100

    feat: add todos' dao and model

---
+++ src/main/java/be/soryo/domain/models/Todo.java
@@ -0,0 +1,40 @@
+package be.soryo.domain.models;
+
+import io.vertx.mutiny.sqlclient.Row;
+
+public class Todo {
+  private Long id;
+  private String title;
+  private boolean completed;
+
+  public Todo(Long id, String title, boolean completed) {
+    this.id = id;
+    this.title = title;
+    this.completed = completed;
+  }
+
+  public Todo(String title, boolean completed) {
+    this.title = title;
+    this.completed = completed;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public boolean getCompleted() {
+    return completed;
+  }
+
+  public void setCompleted(boolean completed) {
+    this.completed = completed;
+  }
+
+  public static Todo from(Row row) {
+    return new Todo(row.getLong("id"), row.getString("title"), row.getBoolean("completed"));
+  }
+}
---
+++ src/main/java/be/soryo/todos/infrastructure/pgsql/TodosDAO.java
@@ -0,0 +1,22 @@
+package be.soryo.todos.infrastructure.pgsql;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import be.soryo.domain.models.Todo;
+import io.quarkus.reactive.datasource.ReactiveDataSource;
+import io.smallrye.mutiny.Multi;
+import io.vertx.mutiny.pgclient.PgPool;
+
+@ApplicationScoped
+public class TodosDAO {
+  @Inject
+  @ReactiveDataSource("postgresTodos")
+  PgPool client;
+
+  public Multi<Todo> listTodos() {
+    return client.query("SELECT * FROM todos").execute()
+      .onItem().transformToMulti(set -> Multi.createFrom().iterable(set))
+      .onItem().transform(Todo::from);
+  }
+}
